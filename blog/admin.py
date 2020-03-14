1a32a869de83ef724145b28d264530e4e9340920
LawrenceDavy13 <lawrencedavy13@gmail.com>
2020-03-14 14:29:58 +0000

    comments deleted due to admin error, will add in future

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,15 +8,6 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on', 'active')
-    list_filter = ('active', 'created_on')
-    search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(active=True)
-
   
 admin.site.register(Post, PostAdmin)
-@admin.register(Comment)
+
