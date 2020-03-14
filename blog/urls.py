df87054f875a1396cdf3977331d30392848805b8
LawrenceDavy13 <lawrencedavy13@gmail.com>
2020-03-14 14:26:24 +0000

    trying to add comments to blog, not appearing in admin site

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from views import post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('/', views.post_detail, name='post_detail'),
+
 ]
